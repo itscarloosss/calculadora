c0c9992aad0cac62c8d32a7863e59bb7f8aab5da
DAM1_Alu20 <carlosavispo19@gmail.com>
2023-11-29 21:11:02 +0100

    Luego seguimos el proyecto creando la clase multiplicacion

---
+++ src/Multiplicacion.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+// Clase para la operación de multiplicación
+class Multiplicacion {
+    public static double multiplicar(double num1, double num2) {
+        return num1 * num2;
+    }
+}
